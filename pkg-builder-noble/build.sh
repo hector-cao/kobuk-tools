2c2d85e05668419c46ad49dd900579c4a5b881e8
Hector Cao <hector.cao@canonical.com>
2023-12-13 10:20:03 +0100

    Fix noble

--- pkg-builder-noble/build.sh
+++ pkg-builder-noble/build.sh
@@ -5,18 +5,18 @@ CURR_DIR=$(dirname "$(readlink -f "$0")")
 #sudo apt install -y docker-buildx
 
 if [ "$1" = "all" ]; then
-		docker build -t mantic-pkg-build $CURR_DIR/
+		docker build -t noble-pkg-build $CURR_DIR/
 fi
 
 if [ "$1" = "-i" ]; then
     # in interactive mode, the LANG=C fakeroot debian/rules binary-generic command fails
     # error : stdout is a console, aborting
-    docker run -v ${PWD}:/src  -it --entrypoint="" mantic-pkg-build bash
+    docker run -v ${PWD}:/src  -it --entrypoint="" noble-pkg-build bash
 
 		# inside the shell, to rebuild the kernel, we can use
 		#debuild -us -uc -b
 else
-    docker run -v ${PWD}:/src/ mantic-pkg-build
+    docker run -v ${PWD}:/src/ noble-pkg-build
 fi
 
 
