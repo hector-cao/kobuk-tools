a3de71d01aae5731946c741d126fbd9314c37e1c
Hector Cao <hector.cao@canonical.com>
2024-01-16 00:51:36 +0000

    add focal

--- ubuntu-work/build.sh
+++ ubuntu-work/build.sh
@@ -1,5 +1,7 @@
+
 #!/bin/bash
 
 CURR_DIR=$(dirname "$(readlink -f "$0")")
 
 docker build -t ubuntu-jammy-src -f Dockerfile.jammy $CURR_DIR/
+docker build -t ubuntu-focal-src -f Dockerfile.focal $CURR_DIR/
