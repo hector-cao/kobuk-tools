284da8e1f74c76e7d22063134ebecc76a01cb5af
Hector Cao <hector.cao@canonical.com>
2024-01-17 18:04:27 +0000

    add ubuntu noble src

--- ubuntu-work/build.sh
+++ ubuntu-work/build.sh
@@ -5,3 +5,4 @@ CURR_DIR=$(dirname "$(readlink -f "$0")")
 
 docker build -t ubuntu-jammy-src -f Dockerfile.jammy $CURR_DIR/
 docker build -t ubuntu-focal-src -f Dockerfile.focal $CURR_DIR/
+docker build -t ubuntu-noble-src -f Dockerfile.noble $CURR_DIR/
