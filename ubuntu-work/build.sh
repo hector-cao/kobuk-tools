8adbd79fe1d99138aa612a052f04b25fb2b2ada6
Hector Cao <hector.cao@canonical.com>
2024-02-09 23:48:51 +0100

    add mantic

--- ubuntu-work/build.sh
+++ ubuntu-work/build.sh
@@ -5,4 +5,5 @@ CURR_DIR=$(dirname "$(readlink -f "$0")")
 
 docker build -t ubuntu-jammy-src -f Dockerfile.jammy $CURR_DIR/
 docker build -t ubuntu-focal-src -f Dockerfile.focal $CURR_DIR/
+docker build -t ubuntu-mantic-src -f Dockerfile.mantic $CURR_DIR/
 docker build -t ubuntu-noble-src -f Dockerfile.noble $CURR_DIR/
