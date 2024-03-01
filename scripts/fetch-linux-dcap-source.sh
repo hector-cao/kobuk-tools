49200c3d1af277c16e57121b016c0f08d5ed8588
Hector Cao <hector.cao@canonical.com>
2024-03-01 12:22:51 +0000

    update scripts/fetch-linux-dcap-source.sh

--- scripts/fetch-linux-dcap-source.sh
+++ scripts/fetch-linux-dcap-source.sh
@@ -7,11 +7,13 @@ LINUX_SGX_BRANCH=canonical_packaging_dev
 DCAP_VERSION=1.20
 
 DCAP_SRC_FOLDER_NAME=sgx-dcap-${DCAP_VERSION}
-DCAP_SRC_ARCHIVE_NAME=linux-sgx_${DCAP_VERSION}.orig.tar.gz
+DCAP_SRC_ARCHIVE_NAME=sgx-dcap_${DCAP_VERSION}.orig.tar.gz
 
 prepare_source() {
 		git clone -b canonical_packaging_dev --recursive $LINUX_SGX_URL $DCAP_SRC_FOLDER_NAME
-		#(cd ./$DCAP_SRC_FOLDER_NAME && make preparation)
+		(cd ./$DCAP_SRC_FOLDER_NAME &&
+				 QuoteVerification/prepare_sgxssl.sh nobuild &&
+				 QuoteGeneration/download_prebuilt.sh)
 		tar --exclude-vcs -zcvf $DCAP_SRC_ARCHIVE_NAME $DCAP_SRC_FOLDER_NAME
 }
 
@@ -19,4 +21,4 @@ rm -rf /tmp/sgx-dcap-tmp/
 mkdir -p /tmp/sgx-dcap-tmp/
 cd /tmp/sgx-dcap-tmp/ && prepare_source
 
-echo "linux-sgx : /tmp/sgx-dcap-tmp/$DCAP_SRC_ARCHIVE_NAME"
+echo "sgx-dcap : /tmp/sgx-dcap-tmp/$DCAP_SRC_ARCHIVE_NAME"
