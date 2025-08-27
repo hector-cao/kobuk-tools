11af75e6bacd747e85b963a0404db2af2a3e54c5
Hector Cao <hector.cao@canonical.com>
2025-08-27 11:02:16 +0200

    add questing

---
+++ pkg-builder-questing/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURR_DIR=$(dirname "$(readlink -f "$0")")
+
+#sudo apt install -y docker-buildx
+
+if [ "$1" = "all" ]; then
+		docker build -t plucky-pkg-build $CURR_DIR/
+fi
+
+if [ "$1" = "-i" ]; then
+    # in interactive mode, the LANG=C fakeroot debian/rules binary-generic command fails
+    # error : stdout is a console, aborting
+    docker run -v ${PWD}:/src  -it --entrypoint="" plucky-pkg-build bash
+
+		# inside the shell, to rebuild the kernel, we can use
+		#debuild -us -uc -b
+else
+    docker run -v ${PWD}:/src/ plucky-pkg-build
+fi
+
+
